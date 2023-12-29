99c889e0cb6917f497e63a49328c1cb0d15ea58e
Evgeny Efimenko <evgeny.efimenko@proton.me>
2023-12-30 00:20:56 +0500

    feat: Zen Mode plugin with kitty plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,35 @@
 local default_plugins = {
 
 	{
+		"folke/zen-mode.nvim",
+		opts = {
+			window = {
+				options = {
+					number = false, -- disable number column
+					relativenumber = false, -- disable relative numbers
+				},
+			},
+			plugins = {
+				options = {
+					enabled = true,
+          -- you may turn on/off statusline in zen mode by setting 'laststatus' 
+          -- statusline will be shown only if 'laststatus' == 3
+          laststatus = 0, -- turn off the statusline in zen mode
+				},
+				gitsigns = { enabled = false }, -- disables git signs
+				-- this will change the font size on kitty when in zen mode
+				-- to make this work, you need to set the following kitty options:
+				-- - allow_remote_control socket-only
+				-- - listen_on unix:/tmp/kitty
+				kitty = {
+					enabled = true,
+					font = "+4", -- font size increment
+				},
+			},
+		},
+	},
+
+	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup()
