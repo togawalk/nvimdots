2ba743368fa15322ec89c2b164e9f5b75f611c5a
Evgeny Efimenko <evgeny.efimenko@proton.me>
2024-01-01 16:57:17 +0500

    feat: harmode keymap

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -7,3 +7,9 @@ vim.keymap.set("n", "<leader>e", "<cmd> NvimTreeFocus <CR>", { desc = "Focus nvi
 
 -- -- line numbers
 vim.keymap.set("n", "<leader>rn", "<cmd> ToggleRelativeNumber <CR>", { desc = "Toggle relative number" })
+
+-- harmode
+vim.keymap.set("", "<up>", "<cmd>echo 'Use hjkl, bro' <CR>")
+vim.keymap.set("", "<down>", "<cmd>echo 'Use hjkl, bro' <CR>")
+vim.keymap.set("", "<left>", "<cmd>echo 'Use hjkl, bro' <CR>")
+vim.keymap.set("", "<right>", "<cmd>echo 'Use hjkl, bro' <CR>")
