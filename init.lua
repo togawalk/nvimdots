25ad635e16f3562924da37c0324d16155b5c53cc
Evgeny Efimenko <evgeny.efimenko@proton.me>
2024-01-15 04:06:55 +0500

    chore: fix typo

--- init.lua
+++ init.lua
@@ -4,6 +4,6 @@ require("core.mappings")
 
 --vim.cmd([[colorscheme catppuccin]])
 
--- kanagawa as a backup, `recall()` can return `nil`.
+-- catppuccin as a backup, `recall()` can return `nil`.
 local theme = require("last-color").recall() or "catppuccin"
 vim.cmd(("colorscheme %s"):format(theme))
